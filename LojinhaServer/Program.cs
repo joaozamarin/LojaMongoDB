c0b3cff3eb74786d5e3cfca73bfac7d57479cbf3
joaozamarin <joao.zamarin@gmail.com>
2023-03-06 20:24:00 -0300

    Configurando Cors e MongoDBSettings

--- LojinhaServer/Program.cs
+++ LojinhaServer/Program.cs
@@ -1,6 +1,10 @@
+using LojinhaServer.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureMongoDBSettings(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
